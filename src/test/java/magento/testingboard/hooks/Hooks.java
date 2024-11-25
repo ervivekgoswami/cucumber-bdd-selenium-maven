e6389d9db13ff6eb2b66e20881e57876a8297a53
ervivekgoswami <ervivekgoswami@gmail.com>
2024-11-25 20:02:15 +0530

    Adding After hook

--- src/test/java/magento/testingboard/hooks/Hooks.java
+++ src/test/java/magento/testingboard/hooks/Hooks.java
@@ -2,18 +2,31 @@ package magento.testingboard.hooks;
 
 import java.io.IOException;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import magento.testingboard.base.BaseTestClass;
 
-public class Hooks extends BaseTestClass{
-	
+public class Hooks extends BaseTestClass {
+
 	@Before
 	public void launchApp() throws IOException {
 		openBrowser();
 	}
-	
-	@After
+
+	@After(order = 1)
+	public void takeScraenshotOnFailure(Scenario scenario) {
+		if (scenario.isFailed()) {
+			TakesScreenshot ts = (TakesScreenshot) driver;
+			final byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
+			scenario.attach(screenshot, "image/png", scenario.getName());
+		}
+	}
+
+	@After(order = 0)
 	public void closeApp() {
 		closeDriver();
 	}
